0b16e13ff63f80450aa7f94e27e58f4485a947d7
kirillshilov <sarataza@inbox.ru>
2022-06-26 20:00:09 +0300

    initial

---
+++ src/main/java/sky/Corsework2Application.java
@@ -0,0 +1,13 @@
+package sky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Corsework2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Corsework2Application.class, args);
+    }
+
+}
---
+++ src/test/java/sky/Corsework2ApplicationTests.java
@@ -0,0 +1,13 @@
+package sky;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Corsework2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
